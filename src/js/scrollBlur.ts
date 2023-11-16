5ee7b2cec0fcf6035d146d41f7a2705088198bf8
Tomie Sayaka <tomie_sayaka@giginc.co.jp>
2023-11-16 11:35:04 +0900

    add: renewal

---
+++ src/js/scrollBlur.ts
@@ -0,0 +1,16 @@
+export const scrollBlur = () => {
+  const FV = document.querySelector(".FV") as HTMLElement | null;
+
+  if (FV) {
+    window.addEventListener("scroll", function () {
+      // スクロール位置を取得
+      const scrollPosition = window.scrollY;
+
+      // スクロール位置に基づいてブラーの強度を計算
+      const blurAmount = Math.min(scrollPosition / 100, 10); // 適宜調整してください
+
+      // カスタムプロパティを使用して擬似要素のフィルタープロパティを更新
+      FV.style.setProperty('--blur', `${blurAmount}px`);
+    });
+  }
+};
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./scss/index.scss";
+import { scrollBlur } from "./js/scrollBlur";
+
+window.addEventListener("DOMContentLoaded", () => {
+  scrollBlur();
+});
