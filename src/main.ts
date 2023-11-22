9b4082793ff24e03f9fa302338e4c641c7fb7c1d
Tomie Sayaka <tomie_sayaka@giginc.co.jp>
2023-11-23 06:19:13 +0900

    add:

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./scss/index.scss";
-import { scrollBlur } from "./js/scrollBlur";
-import { scrollFade } from "./js/scrollFade";
-
-window.addEventListener("DOMContentLoaded", () => {
-  scrollBlur();
-  scrollFade();
-});
