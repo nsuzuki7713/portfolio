917025f384202ff534411a36153b506ba1212dfc
Sayaka Tomie <sayakatomie@SayakanoMacBook-Air.local>
2023-12-16 18:17:15 +0900

    update

--- portfolio-page/src/main.tsx
+++ portfolio-page/src/main.tsx
@@ -5,12 +5,5 @@ import App from "./App.tsx";
 import { CustomParticles } from "./js/particles.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <App />
-    <CustomParticles />
-
-    <script src="js/simpleParallax.js"></script>
-    <script src="js/smoothScroll.js"></script>
-    <script src="js/scrollBlue.js"></script>
-  </React.StrictMode>
 );
